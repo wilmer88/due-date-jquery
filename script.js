0d39eb94515be43b86dff72ccc1af7f0ae15f598
wilmerbaby <wilmerbaby@gmail.com>
2020-09-23 16:16:47 -0400

    added the time and date from moment js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("im workin");
+$(document).ready(function () {
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+  var note = document.querySelector("#notes");
+  var nineam = document.querySelector("nine");
+  var savebutton = document.querySelector("#save");
+  var noteform = document.querySelector("#noty");
+  var noteform2 = document.querySelector("#notoo");
+  var savedEvent = [];
+
+  function getsaved() {
+    var savedNote = JSON.parse(localStorage.getItem("9"));
+    noteform.textContent = savedNote;
+    var savedNote2 = JSON.parse(localStorage.getItem("10"));
+    noteform2.textContent = savedNote2;
+  }
+  getsaved();
+  savebutton.addEventListener("click", function (event) {
+    event.preventDefault();
+    var savedItem = $(this).prev().val();
+    console.log(savedItem);
+    localStorage.setItem(this.data, JSON.stringify(savedItem));
+    // var noteText = note.nodeValue.trim();
+    // if(noteText === ""){
+    //     return;
+  });
+});
