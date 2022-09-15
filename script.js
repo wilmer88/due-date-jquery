426d98d20b7bc9b33289404aa7f2100f82bb4437
Wilmer Rivera <wilmerbaby@gmail.com>
2022-09-15 13:17:49 -0400

    finished criteria with all redundant else if statements

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
   var noteform = document.querySelector("#noty");
   var noteform2 = document.querySelector("#notoo");
   var noteform3 = document.querySelector("#note3");
@@ -8,8 +7,67 @@ $(document).ready(function () {
   var noteform5 = document.querySelector("#note5");
   var noteform6 = document.querySelector("#note6");
   var noteform7 = document.querySelector("#note7");
-  
-  
+  var timeVar = moment().format("h a");
+  if (timeVar === "9 am"){
+      noteform.setAttribute("class", "present");
+         
+            noteform2.setAttribute("class", "col-sm-10 future");
+            noteform3.setAttribute("class", "col-sm-10 future");
+            noteform4.setAttribute("class", "col-sm-10 future");
+            noteform5.setAttribute("class", "col-sm-10 future");
+            noteform6.setAttribute("class", "col-sm-10 future");
+            noteform7.setAttribute("class", "col-sm-10 future");
+    } else if (timeVar === "10 am"){
+      noteform2.setAttribute("class", "col-sm-10 present");
+      noteform.setAttribute("class", "col-sm-10 past");
+      noteform3.setAttribute("class", "col-sm-10 future");
+      noteform4.setAttribute("class", "col-sm-10 future");
+      noteform5.setAttribute("class", "col-sm-10 future");
+      noteform6.setAttribute("class", "col-sm-10 future");
+      noteform7.setAttribute("class", "col-sm-10 future");
+    }else if (timeVar === "11 am"){
+      noteform3.setAttribute("class", "col-sm-10 present");
+      noteform.setAttribute("class", "col-sm-10 past");
+      noteform2.setAttribute("class", "col-sm-10 past");
+      noteform4.setAttribute("class", "col-sm-10 future");
+      noteform5.setAttribute("class", "col-sm-10 future");
+      noteform6.setAttribute("class", "col-sm-10 future");
+      noteform7.setAttribute("class", "col-sm-10 future");
+    }else if (timeVar === "12 pm"){
+      noteform4.setAttribute("class", "col-sm-10 present");
+      noteform.setAttribute("class", "col-sm-10 past");
+      noteform2.setAttribute("class", "col-sm-10 past");
+      noteform3.setAttribute("class", "col-sm-10 past");
+      noteform5.setAttribute("class", "col-sm-10 future");
+      noteform6.setAttribute("class", "col-sm-10 future");
+      noteform7.setAttribute("class", "col-sm-10 future");
+    }else if (timeVar === "1 pm"){
+      noteform5.setAttribute("class", "col-sm-10 present");
+      noteform.setAttribute("class", "pcol-sm-10 ast");
+      noteform2.setAttribute("class", "col-sm-10 past");
+      noteform3.setAttribute("class", "col-sm-10 past");
+      noteform4.setAttribute("class", "col-sm-10 past");
+      noteform6.setAttribute("class", "col-sm-10 future");
+      noteform7.setAttribute("class", "col-sm-10 future");
+    }else if (timeVar === "2 pm"){
+      noteform6.setAttribute("class", "col-sm-10 present");
+      noteform.setAttribute("class", "col-sm-10 past");
+      noteform2.setAttribute("class", "col-sm-10 past");
+      noteform3.setAttribute("class", "col-sm-10 past");
+      noteform4.setAttribute("class", "col-sm-10 past");
+      noteform5.setAttribute("class", "col-sm-10 past");
+      noteform7.setAttribute("class", "col-sm-10 future");
+      
+    }else if (timeVar === "3 pm"){
+      noteform7.setAttribute("class", "col-sm-10 present");
+      noteform.setAttribute("class", "col-sm-10 past");
+      noteform2.setAttribute("class", "col-sm-10 past");
+      noteform3.setAttribute("class", "col-sm-10 past");
+      noteform4.setAttribute("class", "col-sm-10 past");
+      noteform5.setAttribute("class", "col-sm-10 past");
+      noteform6.setAttribute("class", "col-sm-10 past");
+      
+    }
 
   function pullingfromlocal() {
     var savedNote = JSON.parse(localStorage.getItem("nueve"));
@@ -29,8 +87,6 @@ $(document).ready(function () {
     
   }
  pullingfromlocal();
-
-
   $(document).on("click","button", function (event) {
     event.preventDefault();
     var savedItem = $(this).prev().val();
